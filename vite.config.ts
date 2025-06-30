ee225d844b077b46302c615883afb18186413627
bramz-00 <zakaria.braham@outlook.com>
2025-06-30 23:22:10 +0100

    correct vite path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+
+import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path" 
+import tailwindcss from "@tailwindcss/vite"
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
