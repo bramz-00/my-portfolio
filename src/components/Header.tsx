804e92425d8f6550407b0c6458c9c8f983996125
bramz-00 <zakaria.braham@outlook.com>
2025-07-05 00:10:35 +0100

    addd logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ import { BsDiagram2 } from "react-icons/bs";
 import { IoSchoolOutline } from "react-icons/io5";
 import { PiContactlessPaymentLight } from "react-icons/pi";
 import { MobileBottomNavbar } from './MobileBottomNavbar';
-
+import logo from "../assets/logo.svg"
 export default function Header() {
     const { t } = useTranslation();
 
@@ -41,25 +41,32 @@ export default function Header() {
         <div className="bg-white justify-center flex items-center w-full">
             <header className={` fixed  items-center   top-0  z-50 transition-all duration-300 ${scrolled ? " shadow lg:mt-4 lg:w-5xl w-full   rounded-b-lg lg:rounded-xl backdrop-blur-2xl transition-all duration-300 bg-white" : "w-full"
                 }`} >
-                <nav aria-label="Global" className="flex items-center justify-between p-4 lg:px-8">
-                    <div className="flex lg:flex-1 w-full items-center justify-between ">
-                        <a href="#" className="-m-1.5 p-1.5">
+                <nav aria-label="Global" className="">
+                    <div className="flex  w-full  items-center  lg:justify-center justify-between  py-3 ">
+                        <div className={`px-6 flex lg:justify-center w-full justify-between  items-center transition-all duration-300  ${scrolled ? "lg:gap-12 gap-2" : "lg:gap-30 gap-2"}`}>
+                            <a href="#" className="-m-1.5 p-1.5">
 
-                            Zakaria Braham
-                        </a>
-                        <div className='flex lg:gap-12  gap-2 items-center '>
+                               <img src={logo} alt="" className={` ${scrolled ?"w-16":""}`} />
+                            </a>
 
 
                             <div className="hidden lg:flex lg:gap-x-7 ">
-                                {navigation.map((item) => (
-                                    <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
-                                        {item.name}
-                                    </a>
-                                ))}
+                                {navigation.map((item) => {
+                                    const IconComponent = item.icon;
+                                    return (
+                                        <a key={item.name} href={item.href} className="hover:text-blue-600 flex text-sm/6 gap-2 items-center font-semibold text-gray-900">
+                                            {item.name}
+                                            <IconComponent className="text-2xl" />
+                                        </a>
+                                    )
+                                })}
                             </div>
-                            <LanguageSwitcher />
-                            <div className="flex lg:hidden">
+                            <div className="flex items-center justify-between gap-3">
+                                <LanguageSwitcher />
+                                <div className="block lg:hidden">
+
                                 <MobileNavbar navigation={navigation} />
+                                </div>
                             </div>
                         </div>
 
