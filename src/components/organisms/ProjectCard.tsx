2a01de005cf9943d675d758d0ad9b65a0a1e5a26
Zakaria <zakaria.braham@outlook.com>
2025-09-26 19:00:41 +0100

    fix bugs

--- src/components/organisms/ProjectCard.tsx
+++ src/components/organisms/ProjectCard.tsx
@@ -1,6 +1,6 @@
-import { ExternalLink, Github } from 'lucide-react';
+// import { ExternalLink, Github } from 'lucide-react';
 import React from 'react'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import type { Project } from "@/types/collection";
 interface ProjectCardProps {
     project: Project;
